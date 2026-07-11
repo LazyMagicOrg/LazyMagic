b54e9ba204eaf75eaed4f856d414e787c759a3be
NoahSunde <noahjsunde@gmail.com>
2026-07-11 13:34:00 -0700

    interim

--- LazyMagic.Client.ViewModels/GlobalUsings.cs
+++ LazyMagic.Client.ViewModels/GlobalUsings.cs
@@ -10,6 +10,7 @@ global using Microsoft.Extensions.Logging;
 global using Newtonsoft.Json;
 global using ReactiveUI;
 global using ReactiveUI.Fody.Helpers;
+
 global using System.ComponentModel;
 global using System.Collections.Specialized;
 global using System.Reactive.Linq;
